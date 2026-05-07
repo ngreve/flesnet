4e8d486196acff4a691c884ce927e50484ca7a98
Nico Greve <greve@zib.de>
2026-05-07 18:13:15 +0200

    tsforwarder: stabilizing async TS sending

--- app/tsclient/Application.cpp
+++ app/tsclient/Application.cpp
@@ -205,8 +205,6 @@ void Application::run() {
       ++index;
       continue;
     }
-    // std::cout << "timeslice->timeslice_descriptor_.num_core_microslices: " << timeslice->timeslice_descriptor_.num_core_microslices << std::endl;
-    // std::cout << "timeslice->num_microslices(0): " << timeslice->num_microslices(0) << std::endl;
 
     std::shared_ptr<const fles::Timeslice> ts;
     if (par_.release_mode()) {
@@ -215,8 +213,6 @@ void Application::run() {
     } else {
       ts = std::shared_ptr<const fles::Timeslice>(std::move(timeslice));
     }
-    // std::cout << "ts->timeslice_descriptor_.num_core_microslices: " << ts->timeslice_descriptor_.num_core_microslices << std::endl;
-    // std::cout << "ts->num_microslices(0): " << ts->num_microslices(0) << std::endl;
 
     if (par_.native_speed() != 0.0) {
       native_speed_delay(ts->start_time());
@@ -224,7 +220,25 @@ void Application::run() {
     if (par_.rate_limit() != 0.0) {
       rate_limit_delay();
     }
-    // ts->descriptor()
+
+                    for (uint64_t c = 0; c < ts->num_components(); c++) {
+                    if (ts->size_component(c) == 0) {
+                        std::cout << "is 0" << std::endl;
+                        exit(-1);
+                    }
+                    for (uint64_t m = 0;  m < ts->num_microslices(c); m++) {
+                        auto m_desc = ts->descriptor(c, m);
+                        if (m_desc.size == 0) {
+                            std::cout << "component: " << c << std::endl;
+                            std::cout << "ms idx: " << m << std::endl;
+                            std::cout << "m_desc.sys_id: " << uint64_t(m_desc.sys_id) << std::endl;
+                            std::cout << "m_desc.eq_id: " << uint64_t(m_desc.eq_id) << std::endl;
+                            std::cout << "m_desc.flags: " << uint64_t(m_desc.flags) << std::endl;
+                            std::cout << "microslice == 0" << std::endl;
+                            exit(-1);
+                        }
+                    }
+                }
     for (auto& sink : sinks_) {
       sink->put(ts);
     }
