236de9c5033122fdc5ea6c0488c4b50e06c19b2c
Nico Greve <greve@zib.de>
2026-08-04 10:23:55 +0200

    Moved fields to protected to derive from certain classes
    - Moved fields and methods in Timeslice, TimesliceReceiver and
      TimesliceView to protected to give access to these fields to derived
      classes used in the timeslice forwarder.

--- lib/fles_ipc/TimesliceReceiver.hpp
+++ lib/fles_ipc/TimesliceReceiver.hpp
@@ -72,6 +72,23 @@ public:
 
 protected:
   std::shared_ptr<boost::interprocess::managed_shared_memory> managed_shm_;
+  /// The end-of-stream flag.
+  bool eos_ = false;
+
+  // The respective item worker object
+  ItemWorker worker_;
+
+  [[nodiscard]] boost::uuids::uuid managed_shm_uuid() const {
+    if (!managed_shm_) {
+      return boost::uuids::nil_uuid();
+    }
+    auto* shm_uuid =
+        managed_shm_
+            ->find<boost::uuids::uuid>(boost::interprocess::unique_instance)
+            .first;
+    assert(shm_uuid != nullptr);
+    return *shm_uuid;
+  }
 
 private:
   TimesliceView* do_get() override {
@@ -129,23 +146,6 @@ private:
     return nullptr;
   }
 
-  [[nodiscard]] boost::uuids::uuid managed_shm_uuid() const {
-    if (!managed_shm_) {
-      return boost::uuids::nil_uuid();
-    }
-    auto* shm_uuid =
-        managed_shm_
-            ->find<boost::uuids::uuid>(boost::interprocess::unique_instance)
-            .first;
-    assert(shm_uuid != nullptr);
-    return *shm_uuid;
-  }
-
-  /// The end-of-stream flag.
-  bool eos_ = false;
-
-  // The respective item worker object
-  ItemWorker worker_;
 
   /// The access mode for the shared memory segment
   ShmAccess access_;
